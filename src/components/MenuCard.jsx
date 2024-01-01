5502771a840b1cccc1dd2f6e2aba42331bc8bd1c
ganpatsundesha <ganpatsundesha47@gmail.com>
2024-01-01 18:53:15 +0530

    UI(css) changes

--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -12,7 +12,7 @@ const MenuCard = ({ menuData }) => {
                         return (
                             <div className='card-box' key={index}>
                                 <span>{index + 1}</span>
-                                <div>{category}</div>
+                                <div className='category'>{category}</div>
                                 <div className='name'>{itemName}</div>
                                 <img src={imageUrl} alt="" />
                                 <div className='discription'>{discription}</div>
